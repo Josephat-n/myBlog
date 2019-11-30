bdb5cd940f609caa812af51bdd4519ad7c574193
Josephat-n <josphatnjoroge254@gmail.com>
2019-11-30 17:46:36 +0300

    create a route to allow commenting on a blog, and the blop page for so.

--- app/main/views.py
+++ app/main/views.py
@@ -10,3 +10,14 @@ def index():
    blogs = Blog.query.all()
    print(blogs)
    return render_template('index.html', blogs = blogs)
+
+@main.route('/blog/<int:id>')
+def blog(id):
+   """
+   Allow for commenting of a given blog.
+   """
+   #Should return a blog by id   
+   a_blog = Blog.query.filter_by(id= id)
+   print(a_blog)
+   
+   return render_template('blog.html', id = id, a_blog = a_blog)
