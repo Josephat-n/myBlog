dbc665e1b35aafffce9aa61030fb47570c0bc6b8
Josephat-n <josphatnjoroge254@gmail.com>
2019-12-03 08:42:55 +0300

    heroku upload

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
 	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):   
-   pass
+   SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
    
 class DevConfig(Config):  
    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://njoro:njoro@localhost/blog' 
--- tests/test_user.py
+++ tests/test_user.py
@@ -8,4 +8,25 @@ class UserModelTest(unittest.TestCase):
    def setUp(self):
       self.new_user = User("1","josphat","josphatmax@gmail.com", "1234", "yudbc", "this is a great article") #create a user object
    
-      
+   def test_setting_password(self):
+       '''
+    testcase to test on setting a new password
+    '''
+    self.assertFalse(self.new_user.pass_secure is None)
+
+  def test_no_access_to_passwd(self):
+    '''
+    testcase to test that no can access a password from the database
+    '''
+    if self.new_user.pass_secure:
+      return self.assertRaises(AttributeError)
+
+    else:
+      return self.assertRaises('False')  
+
+  def test_password_verify(self):
+    '''
+    testcase to check verifying of hashed password  to login a user
+    '''
+    self.assertTrue(self.new_user.verify_password,('1234'))    
+   
